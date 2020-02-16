45ea1facd1d87397568f3adbf0f9668c032f6bd6
Kerosien <49041037+Kerosien@users.noreply.github.com>
2020-02-16 03:22:04 -0800

    Shooting controls
    Attempting to add the shooting controls not in place yet

---
+++ Project Ox/Assets/Scripts/WeaponController2D.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponController2D : MonoBehaviour
+{
+    public Transform firePoint;
+    public int damage;
+    public Joystick joystick;
+    //public GameObject impactEffect;
+    public LineRenderer lineRenderer;
+    public Button shootButton;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (shootButton.enabled)
+        {
+            StartCoroutine(Shoot());
+        }
+    }
+
+    IEnumerator Shoot ()
+    {
+        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, firePoint.right);
+        
+        if (hitInfo)
+        {
+            Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+
+            //Instantiate(impactEffect, hitInfo.point, Quaternion.identity);
+
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, hitInfo.point);
+
+        }
+        else
+        {
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, firePoint.position + firePoint.right * 100);
+        }
+
+        lineRenderer.enabled = true;
+
+        yield return 0;
+
+        lineRenderer.enabled = false;
+    }
+}
