9b808fbebbbaba9bd634ae02e4ac586ac7b4f137
Kerosien <49041037+Kerosien@users.noreply.github.com>
2020-02-15 22:31:16 -0800

    Project Start
    I moved over everything that I have done for the start of the game. From here we will build an Android twin stick shooter with ads. All changes must be agreed apon by everyone before commiting to the Master branch

---
+++ Project Ox/Assets/Scripts/PlayerMovement2D.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement2D : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    public Joystick joystick;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    void Update()
+    {
+        //horizontalMove = joystick.Horizontal * runSpeed;       I like this movement alot but will flesh it out before choosing
+
+        if (joystick.Horizontal >= .2f)
+        {
+            horizontalMove = runSpeed;
+        }
+        else if (joystick.Horizontal <= -.2f)
+        {
+            horizontalMove = -runSpeed;
+        }
+        else
+        {
+            horizontalMove = 0f;
+        }
+
+        float verticalMove = joystick.Vertical;
+
+        if (verticalMove >= .5f)
+        {
+            jump = true;
+        }
+
+        if (verticalMove <= -.5f)
+        {
+            crouch = true;
+        }
+
+        else
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+
+    }
+}
